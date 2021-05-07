a3c5401005b608422b536f9daef1f21b71484aac
Ramadan Ibrahem <61190974+RamadanIbrahem98@users.noreply.github.com>
2021-05-07 22:06:26 +0200

    add fft function

--- main.cpp
+++ main.cpp
@@ -8,6 +8,36 @@
 extern "C"
 {
     void dft(std::complex<double>* input, std::complex<double>* output, const int size);
+    void fft(std::complex<double>* x, int size);
+}
+
+void fft(std::complex<double>* x, int size)
+{
+	const size_t N = size;
+	if (N <= 1) return;
+
+    int M = size/2;
+    
+    std::complex<double>* even = new std::complex<double>[M];
+    std::complex<double>* odd = new std::complex<double>[M];
+
+    for(int i = 0; i < M; i++)
+    {
+        even[i] = x[2*i];
+        odd [i] = x[2*i + 1];
+    }
+
+	// conquer
+	fft(even, M);
+	fft(odd, M);
+
+	// combine
+	for (size_t k = 0; k < N / 2; ++k)
+	{
+		std::complex<double> t = std::polar(1.0, -2 * M_PI * k / N) * odd[k];
+		x[k    ]     = even[k] + t;
+		x[k + N / 2] = even[k] - t;
+	}
 }
 
 void dft(std::complex<double>* input, std::complex<double>* output, const int size)
